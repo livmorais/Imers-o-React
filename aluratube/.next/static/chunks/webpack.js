ea261ffa10811e6b3ed93485e38bd0ef799ba763
livmorais <liviasantosmorais@gmail.com>
2022-11-10 15:50:46 -0300

    adicionando formulario

--- aluratube/.next/static/chunks/webpack.js
+++ aluratube/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cd322d5ecc0e0e7"; }
+/******/ 		__webpack_require__.h = function() { return "dec6e7482e08ba8f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
